4efca05c7bde3eee6913981a96fbb6c2dca79300
mccleskeyc <mccleskeyc@gmail.com>
2020-11-18 10:55:29 -0500

    commit tree bef4d43ed34b2ea22b93551e20b0892a297fd26e

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+    @roster[grade] << name
+    end
+
+    def grade(grade_year)
+        @roster[grade_year]
+    end
+
+    def sort
+        @roster.each do |grade, name|
+            @roster[grade] = name.sort
+        end
+    end
+end
